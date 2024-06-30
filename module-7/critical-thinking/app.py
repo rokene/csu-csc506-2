ef4ede724893b06610f7e6b27a492842fc68ea32
Allen Choi <mr.allen.choi@gmail.com>
2024-06-30 17:33:56 +0000

    Update docs and use heapq

--- module-7/critical-thinking/app.py
+++ module-7/critical-thinking/app.py
@@ -20,31 +20,28 @@ class Graph:
         self.distances[(to_node, from_node)] = distance
 
 def dijkstra(graph, initial):
-    visited = {initial: 0}
+    # Priority queue to store (distance, node) tuples
+    priority_queue = []
+    heapq.heappush(priority_queue, (0, initial))
+
+    visited = {}
     path = {}
 
-    nodes = set(graph.nodes)
+    while priority_queue:
+        current_distance, min_node = heapq.heappop(priority_queue)
 
-    while nodes:
-        min_node = None
-        for node in nodes:
-            if node in visited:
-                if min_node is None:
-                    min_node = node
-                elif visited[node] < visited[min_node]:
-                    min_node = node
+        if min_node in visited:
+            continue
 
-        if min_node is None:
-            break
+        visited[min_node] = current_distance
 
-        nodes.remove(min_node)
-        current_weight = visited[min_node]
+        for neighbor in graph.edges[min_node]:
+            weight = graph.distances[(min_node, neighbor)]
+            distance = current_distance + weight
 
-        for edge in graph.edges[min_node]:
-            weight = current_weight + graph.distances[(min_node, edge)]
-            if edge not in visited or weight < visited[edge]:
-                visited[edge] = weight
-                path[edge] = min_node
+            if neighbor not in visited:
+                heapq.heappush(priority_queue, (distance, neighbor))
+                path[neighbor] = min_node
 
     return visited, path
 
